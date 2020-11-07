21cdb47d704ff78f7d45b8dcb2510c0951f6ce14
Aditya Das <aditya.tech01@gmail.com>
2020-11-07 18:56:15 +0530

    changed acccess specifiers

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,4 +1,4 @@
 public class Utils {
-    final static String BASE_URL = "http://localhost:8080/hello";
-    final static String CHROME_DRIVER_LOCATION = "src/test/drivers/chromedriver";
+    public final static String BASE_URL = "http://localhost:8080/hello";
+    public final static String CHROME_DRIVER_LOCATION = "src/test/drivers/chromedriver";
 }
