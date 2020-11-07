6f83eaaf89e8e2a071c49c13d46ebe2b04647989
Aditya Das <aditya.tech01@gmail.com>
2020-11-07 19:46:05 +0530

    added ChromeOptions

--- src/test/java/TestPlan.java
+++ src/test/java/TestPlan.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class TestPlan {
-    private static final WebDriver driver = new ChromeDriver();
+
+    private static final WebDriver driver = new ChromeDriver(Utils.returnChromeOptions());
 
     @BeforeSuite
     public static void main(String[] args) {
--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,4 +1,17 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Utils {
     public final static String BASE_URL = "http://localhost:8080/hello";
     public final static String CHROME_DRIVER_LOCATION = "src/test/drivers/chromedriver";
+
+    public static ChromeOptions returnChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        return options;
+    }
 }
